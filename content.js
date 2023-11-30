c296d537442722cd1bab635c2620c2fa3901bba7
salisbrach <132305573+salisbrach@users.noreply.github.com>
2023-11-29 21:25:48 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,19 @@
+function copyTextToClipboard(text) {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "copyText") {
+        const selectedText = window.getSelection().toString();
+        const url = window.location.href;
+        const currentDate = new Date().toISOString().split('T')[0];
+        const formattedText = `> ${selectedText}\n\n[${currentDate}](${url})`;
+        copyTextToClipboard(formattedText);
+    }
+});
 
