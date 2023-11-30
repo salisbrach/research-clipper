b325fd6a45123b9e36c7e4b013d11f810de5aede
salisbrach <132305573+salisbrach@users.noreply.github.com>
2023-11-29 21:36:33 -0800

    update to manifest v3

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
 chrome.commands.onCommand.addListener(function (command) {
     if (command === "copy-command") {
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "copyText"});
+            chrome.scripting.executeScript({
+                target: {tabId: tabs[0].id},
+                function: copySelectedText
+            });
         });
     }
 });
+
+function copySelectedText() {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "copyText"});
+}
